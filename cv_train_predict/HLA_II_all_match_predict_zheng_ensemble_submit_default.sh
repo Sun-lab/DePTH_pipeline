05f3b239018a750bd1d62591bcfd8943131ab3ef
liusi2019 <liusi2019@gmal.com>
2023-03-24 18:47:17 -0700

    minor correction

--- cv_train_predict/HLA_II_all_match_predict_zheng_ensemble_submit_default.sh
+++ cv_train_predict/HLA_II_all_match_predict_zheng_ensemble_submit_default.sh
@@ -3,4 +3,4 @@
 
 DePTH predict --test_file data/Zheng_2021/zheng_2021_HLA_II_pairs.csv \
               --hla_class HLA_II \
-              --output_dir results/ensemble_scores/HLA_II_all_match_zheng_default/
+              --output_dir results/ensemble_scores/HLA_II_all_match_zheng_default
