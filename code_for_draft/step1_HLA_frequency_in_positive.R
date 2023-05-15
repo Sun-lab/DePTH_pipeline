cbe486adecf95d1ea31ab69b6de453426b107fa6
liusi2019 <liusi2019@gmal.com>
2023-05-14 18:31:54 -0700

    minor modification to file step1_HLA_frequency_in_positive.R

--- code_for_draft/step1_HLA_frequency_in_positive.R
+++ code_for_draft/step1_HLA_frequency_in_positive.R
@@ -1,17 +1,18 @@
 
 library(ggplot2)
 
-pos_file = "../data/step77_HLA_I_associated_TCR_v_alleles.csv"
+pos_file = "../data/intermediate_data/t12_HLA_I_associated_TCR_v_alleles.csv"
 df_pos = read.csv(pos_file, header = TRUE)
 dim(df_pos)
 head(df_pos)
 
 df_freq = data.frame(table(df_pos$hla_allele))
+df_freq[order(df_freq$Freq, decreasing = TRUE), ]
 
 p1 <- ggplot(df_freq, aes(x=Freq))+
       geom_histogram(color="darkblue", fill="lightblue") + 
       theme_classic() + xlab("freq among pos pairs")
 
-pdf(file = "../figures/depth_draft/step2_freq_among_positive_pairs.pdf", width = 2.6, height = 2)
+pdf(file = "../figures/depth_draft/step1_freq_among_positive_pairs.pdf", width = 2.6, height = 2)
 print(p1)
 dev.off()
